7f1b0d4a6b9578550ed39e95e358ec3f7cf2b9b9
Николай Колганов <42237828+Espanol-nikkol@users.noreply.github.com>
2020-06-13 12:17:31 +1000

    added find country

--- components/country.js
+++ components/country.js
@@ -2,12 +2,18 @@ import React, {Component} from 'react';
 import {StyleSheet, Button, FlatList} from 'react-native';
 import Constant from './const';
 import RealmDB from './realmDB';
+import SearchBar from 'react-native-search-bar';
 
 class Country extends Component {
+  state = {
+    data: this.listCountry,
+  };
+
   listCountry = (() =>
     Object.keys(Constant.COUNTRY_TO_ISO).map(i => {
       let temp = {};
-      temp[i] = Constant.COUNTRY_TO_ISO[i];
+      temp.id = Constant.COUNTRY_TO_ISO[i];
+      temp.name = i;
       return temp;
     }))();
 
@@ -15,22 +21,38 @@ class Country extends Component {
     RealmDB.setChoosenCountry(id).then(() => props.navigation.navigate('Main'));
   };
 
+  searchFilterFunction = text => {
+    const newData = this.listCountry.filter(item =>
+      item.name.toUpperCase().startsWith(text.toUpperCase()),
+    );
+    this.setState({data: newData});
+  };
 
+  renderHeader = () => {
+    return (
+      <SearchBar
+        placeholder="Country or region Here..."
+        lightTheme
+        round
+        onChangeText={text => this.searchFilterFunction(text)}
+        autoCorrect={false}
+      />
+    );
+  };
 
   render() {
     return (
       <FlatList
         onEndReachedThreshold={0.7}
-        data={this.listCountry}
+        data={this.state.data}
         renderItem={({item}) => (
           <Button
-            title={Object.keys(item)[0]}
-            onPress={() =>
-              this.onClickCountry(item[Object.keys(item)[0]], this.props)
-            }
+            title={item.name}
+            onPress={() => this.onClickCountry(item.id, this.props)}
           />
         )}
-        keyExtractor={item => item[Object.keys(item)[0]]}
+        keyExtractor={item => item.id}
+        ListHeaderComponent={this.renderHeader}
       />
     );
   }
